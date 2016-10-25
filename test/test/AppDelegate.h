543a993bdaa1cc0d081106ca2434eb7f31c3da4e
刘建LJ <liujian@chinamobo.com>
2016-10-25 14:46:56 +0800

    init code

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by liujian on 2016/10/25.
+//  Copyright © 2016年 Chinamobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
